5a0efa1a520d4bf51a5d85d1c40b38b4f820c3cf
Isiah Shelton <81324272+Blobfishro1212@users.noreply.github.com>
2023-01-16 23:35:59 -0800

    Add files via upload

---
+++ TopDownMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    public float speed = 5f; //Speed of the player
+    public Rigidbody2D rb; //Reference to the player's rigidbody
+    Vector2 movement; //Stores the player's movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Get input from the player
+        movement.x = Input.GetAxisRaw("Horizontal"); //Get the horizontal axis
+        movement.y = Input.GetAxisRaw("Vertical"); //Get the vertical axis
+    }
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Move the player
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
